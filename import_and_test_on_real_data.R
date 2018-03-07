fcfb1c7d7e0464b7b17fb4cc6f6e110b8744543e
Dana <dmgt@users.noreply.github.com>
2018-03-06 20:43:41 -0800

    add note about chr type in output, rename for clarity

--- import_and_test_on_real_data.R
+++ import_and_test_on_real_data.R
@@ -2,11 +2,13 @@ library(tidyverse)
 
 #Temporarily using manual columnnames to demonstrate, since orignial column names include encoding that gave error
 # error was not not solved by locale = locale(encoding = "UTF-8")
-vav <- read_csv("sample_data/VAV 2-01.csv", na = "nan", skip = 1,  col_names = c("Time", "DamperPos", "SpaceTemp"))
+raw_vav <- read_csv("sample_data/VAV 2-01.csv", na = "nan", skip = 1,  col_names = c("Time", "DamperPos", "SpaceTemp")) 
     
-head(vav)
-summary(vav)
+head(raw_vav)
+summary(raw_vav)
 
-vav_subset <- vav[1:10,]
+raw_vav_subset <- raw_vav[1:10,]
 
-transform_diagonal_data(vav_subset)
+#Note that this outputs a tibble with the Time column as type chronological
+vav <- transform_diagonal_data(raw_vav_subset)
+vav
